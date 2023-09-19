ff5c8da7ff557e7f1b0cd3ac984d811131d2f1d8
patrick-codes <khofilyndon2018@gmail.com>
2023-09-19 13:41:59 +0000

    fixing db uri issues

--- Config/db_config.js
+++ Config/db_config.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 module.exports = {
   connectDatabase: async (req, res) => {
     try {
+      //mongodb+srv://khofilyndon2018:nvQRNQcqteaKQQHy@cluster0.5igcrnx.mongodb.net/?retryWrites=true&w=majority
       var db = process.env.MONGO_URI;
       const con = await mongoose
-        .connect(`${db}`, {
+        .connect(`${process.env.MONGO_URI}`, {
           useNewUrlParser: true,
           useUnifiedTopology: true,
         })
